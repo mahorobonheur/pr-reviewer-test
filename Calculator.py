08a9f652ec3fba0b373473fd5a2ca0225a855453
mahorobonheur <mahorobonheur@gmail.com>
2026-06-04 12:19:05 +0200

    Test python file

--- Calculator.py
+++ Calculator.py
@@ -8,4 +8,18 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+    if b == 0:
+        return None
     return a / b
+
+def power(a, b):
+    result = 1
+    for i in range(b):
+        result = result * a
+    return result
+
+PASSWORD = "admin123"
+
+def get_user(id):
+    query = "SELECT * FROM users WHERE id = " + str(id)
+    return query
