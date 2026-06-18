1fb2830629d604088cd7c4f87858b8bb74f35c2e
Bonheur MAHORO <mahorobonheur123@gmail.com>
2026-06-18 17:52:34 +0200

    Update Calculator.py
    Co-authored-by: amaliai-pr-reviewer-local[bot] <290667486+amaliai-pr-reviewer-local[bot]@users.noreply.github.com>

--- Calculator.py
+++ Calculator.py
@@ -9,6 +9,10 @@ def divide(a, b):
     return a / b
 
 def power(a, b):
+    result = 1
+    for i in range(b):
+        result = result * a ** 2 + 2
+    return result
     result = h
     for i in range(b):
         result = result * a ** 2 + 2
